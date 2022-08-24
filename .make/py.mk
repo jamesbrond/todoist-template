0afa594112ddccfc6b6aa28396f0226041957194
jamesbrond <diego.brondo@philips.com>
2022-08-24 09:36:20 +0200

    --amend

--- .make/py.mk
+++ .make/py.mk
@@ -1,3 +1,5 @@
+# requires misc.mk
+
 VENV_DIR      := venv
 
 ACTIVATE      := $(VENV_DIR)/Scripts/activate
@@ -8,37 +10,35 @@ pyenv         = $(do_activate) && $(1)
 
 install_deps  = $(do_activate) && pip install -Ur $(1)
 
+.PHONY: clean-pycache clean-venv deps lint
+
 $(ACTIVATE): ## Create python virtual environment
 # The venv module provides support for creating lightweight "virtual environments" with
 # their own site directories, optionally isolated from system site directories.
 # https://docs.python.org/3/library/venv.html
-	@$(call echoclr,$(BWHITE),Creating virtual environment...)
+	@$(call prompt-log,Creating virtual environment)
 	@python -m venv $(VENV_DIR)
 # convert CRLF to LF in activate bash script
 	@sed -i $$'s/\\r$$//' $(ACTIVATE)
-	@$(call echoclr,$(GREEN),Done)
 
 clean-pycache: ## Remove bytecode-compiled python files
-	@$(call echoclr,$(BLACK),Removing bytecode-compiled python files...)
+	@$(call prompt-log,Removing bytecode-compiled python files)
 	@/usr/bin/find $(LIB_DIR) -name __pycache__ -type d  -print0 | xargs -0 -r rm -rf
-	@$(call echoclr,$(GREEN),Done)
 
 clean-venv: ## Remove virtual evnironemnt
-	@$(call echoclr,$(BLACK),Removing virtual environment...)
+	@$(call prompt-log,Removing virtual environment)
 	@rm -rf $(VENV_DIR)
 
 deps: $(ACTIVATE) $(REQ) ## Activate venv and install requirements
-	@$(call echoclr,$(BWHITE),Upgrading pip)
+	@$(call prompt-info,Upgrading pip)
 	@$(call pyenv,python -m pip install --upgrade pip)
-	@$(call echoclr,$(GREEN),Done)
-	@$(call echoclr,$(BWHITE),Installing requirements)
+	@$(call prompt-info,Installing dependencies)
 	@$(call install_deps, $(REQ))
-	@$(call echoclr,$(GREEN),Done)
 
 lint:  ## Lint and static-check
-	@$(call echoclr,$(BWHITE),Running flake8)
-	@$(call pyenv,python -m flake8 $(PACKAGE).py $(LIB_DIR)) && $(call echoclr,$(GREEN),Done) || $(call echoclr,$(RED),Failed)
-	@$(call echoclr,$(BWHITE),Running pylint)
-	@$(call pyenv,python -m pylint --recursive=y $(PACKAGE) $(LIB_DIR) && echo -e "$(GREEN)Done$(COLOR_OFF)" || echo -e "$(RED)Failed$(COLOR_OFF)")
+	@$(call prompt-info,Running flake8)
+	@$(call pyenv,python -m flake8 $(PACKAGE).py $(LIB_DIR)) && $(call prompt-success,Done) || $(call prompt-error,Failed)
+	@$(call prompt-info,Running pylint)
+	@$(call pyenv,python -m pylint --recursive=y $(PACKAGE) $(LIB_DIR)) && $(call prompt-success,Done) || $(call prompt-error,Failed)
 
 # ~@:-]
--- Makefile
+++ Makefile
@@ -1,6 +1,5 @@
-include .make/colors.mk
-include .make/py.mk
 include .make/misc.mk
+include .make/py.mk
 
 PACKAGE       := todoist_template
 
@@ -10,7 +9,7 @@ SRCS_DIR      := .
 DEVREQ        := devrequirements.txt
 VERSION_PY    := $(LIB_DIR)/__version__.py
 
-
+.PHONY: clean devdeps
 .DEFAULT_GOAL := help
 
 version       = $$($(VERSION_PY))
@@ -18,8 +17,7 @@ version       = $$($(VERSION_PY))
 clean: clean-venv clean-pycache ## Clean-up the solution
 
 devdeps: deps ## Install both application and developer requirements (pylint, flask8)
-	@$(call echoclr,$(BWHITE),Installing developement requirements)
+	@$(call prompt-info,Installing developement requirements)
 	@$(call install_deps, $(DEVREQ))
-	@$(call echoclr,$(GREEN),Done)
 
 # ~@:-]
