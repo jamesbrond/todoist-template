2743f78950ec9b0b46422e397f5e9b34b56dc585
James Brond <jamesbrond@gmail.com>
2023-03-13 16:45:38 +0100

    fix: find needle in haystack in object and dict

--- lib/utils.py
+++ lib/utils.py
@@ -9,8 +9,13 @@ def find_needle_in_haystack(needles, haystack, params):
         return None
     query = dict(zip(params, needles))
     for straw in haystack:
-        if all(str(getattr(straw, k)) == v for k, v in query.items()):
-            return straw
+        if isinstance(straw, dict):
+            if all(str(straw.get(k)) == str(v) for k, v in query.items()):
+                return straw
+        else:
+            if all(str(getattr(straw, k)) == str(v) for k, v in query.items()):
+                return straw
+
     return None
 
 
